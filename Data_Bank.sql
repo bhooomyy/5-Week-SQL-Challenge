c12fad2302ff05cc89f58ff91ae3aa599181a548
bhooomyy <bhattbhoomi0111@gmail.com>
2026-02-25 18:03:46 -0500

    Compute average customer node reallocation days
    - Filtered out active allocations (end_date != '9999-12-31').
    - Calculated days per allocation (end_date - start_date).
    - Summed days per customer-node pair.
    - Computed average of these totals across all customers.

--- Data_Bank.sql
+++ Data_Bank.sql
@@ -17,3 +17,19 @@ count(distinct customer_id) as cnt_node
 from customer_nodes c join regions r on c.region_id=r.region_id
 group by r.region_name;
 
+--4. How many days on average are customers reallocated to a different node?
+with table1 as (select
+customer_id,
+node_id,
+end_date-start_date as days_total
+from customer_nodes 
+where end_date!='9999-12-31'),
+
+table2 as(select
+          customer_id, 
+          node_id, 
+          sum(days_total) as sum_total 
+          from table1 
+          group by customer_id,node_id)
+     
+select round(avg(sum_total)) as avg_node_reallocation_days from table2;
