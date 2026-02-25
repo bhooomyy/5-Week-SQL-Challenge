d325a67c91090c97cfa429284cabe82735633c05
bhooomyy <bhattbhoomi0111@gmail.com>
2026-02-25 18:02:15 -0500

    Count nodes per region
    - Joined customer_nodes with regions table.
    - Counted distinct node_id for each region_name.

--- Data_Bank.sql
+++ Data_Bank.sql
@@ -2,3 +2,10 @@
 select 
 count(distinct node_id) 
 from customer_nodes;
+
+-- 2. What is the number of nodes per region?
+select 
+r.region_name,
+count(distinct node_id) as cnt_node 
+from customer_nodes c join regions r on c.region_id=r.region_id
+group by r.region_name;
